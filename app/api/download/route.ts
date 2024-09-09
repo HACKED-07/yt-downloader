c4cb145bf4267198c6811d3d5c236fd5e0f15cb9
Hacked <hacked@HRISHIKESHs-MacBook-Air.local>
2024-09-09 20:17:57 +0530

    Deploying

--- app/api/download/route.ts
+++ app/api/download/route.ts
@@ -1,39 +1,48 @@
+import { NextRequest, NextResponse } from "next/server";
 import ytdl from "@distube/ytdl-core";
 
-export async function POST(request: Request) {
+export async function POST(request: NextRequest) {
   try {
     const data = await request.json();
     const url = data.body.url;
 
     if (!ytdl.validateURL(url)) {
-      return Response.json({ msg: "Invalid YouTube URL" }, { status: 400 });
+      console.error("Invalid YouTube URL:", url);
+      return NextResponse.json({ msg: "Invalid YouTube URL" }, { status: 400 });
     }
 
     const info = await ytdl.getInfo(url);
     const format = ytdl.chooseFormat(info.formats, { quality: "18" });
 
-    const chunks: any[] = [];
+    // Create a Readable stream
     const videoStream = ytdl(url, { format: format });
 
-    // Collect video data into chunks
-    videoStream.on("data", (chunk) => chunks.push(chunk));
-    await new Promise((resolve, reject) => {
-      videoStream.on("end", resolve);
-      videoStream.on("error", reject);
+    // Create a readable stream and a custom Response
+    const stream = new ReadableStream({
+      start(controller) {
+        videoStream.on("data", (chunk) => {
+          controller.enqueue(chunk);
+        });
+        videoStream.on("end", () => {
+          controller.close();
+        });
+        videoStream.on("error", (err) => {
+          console.error("Stream error:", err);
+          controller.error(err);
+        });
+      },
     });
 
-    const buffer = Buffer.concat(chunks);
-
-    return new Response(buffer, {
+    return new Response(stream, {
       headers: {
         "Content-Type": `video/${format.container}`,
         "Content-Disposition": `attachment; filename="${info.videoDetails.title}.${format.container}"`,
       },
     });
   } catch (error) {
-    console.error("Error:", error);
-    return Response.json(
-      { msg: "Error occurred", error },
+    console.error("Error occurred during video download:", error);
+    return NextResponse.json(
+      { msg: "Error occurred" },
       { status: 500 }
     );
   }
