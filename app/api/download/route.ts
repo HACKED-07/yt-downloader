eef672c9597f29f8f81810812446cdf605f653fd
Hacked <hacked@HRISHIKESHs-MacBook-Air.local>
2024-09-09 19:56:19 +0530

    Trying deploment

--- app/api/download/route.ts
+++ app/api/download/route.ts
@@ -3,7 +3,7 @@ import ytdl from "@distube/ytdl-core";
 export async function POST(request: Request) {
   try {
     const data = await request.json();
-    const url = data.body.url; // Get the Video URL
+    const url = data.body.url;
 
     if (!ytdl.validateURL(url)) {
       return Response.json({ msg: "Invalid YouTube URL" }, { status: 400 });
@@ -11,26 +11,29 @@ export async function POST(request: Request) {
 
     const info = await ytdl.getInfo(url);
     const format = ytdl.chooseFormat(info.formats, { quality: "18" });
+
+    const chunks: any[] = [];
     const videoStream = ytdl(url, { format: format });
-    const stream = new ReadableStream({
-      start(controller) {
-        videoStream.on("data", (chunk) => controller.enqueue(chunk));
-        videoStream.on("end", () => controller.close());
-        videoStream.on("error", (err) => controller.error(err));
-      },
+
+    // Collect video data into chunks
+    videoStream.on("data", (chunk) => chunks.push(chunk));
+    await new Promise((resolve, reject) => {
+      videoStream.on("end", resolve);
+      videoStream.on("error", reject);
     });
 
-    return new Response(stream, {
+    const buffer = Buffer.concat(chunks);
+
+    return new Response(buffer, {
       headers: {
         "Content-Type": `video/${format.container}`,
-        "Content-Disposition": "attachment",
-        "X-Video-Title": encodeURIComponent(info.videoDetails.title)
+        "Content-Disposition": `attachment; filename="${info.videoDetails.title}.${format.container}"`,
       },
     });
   } catch (error) {
     console.error("Error:", error);
     return Response.json(
-      { msg: "Error occurred" },
+      { msg: "Error occurred", error },
       { status: 500 }
     );
   }
